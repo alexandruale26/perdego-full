3c62815842efcc2587b7e1a745f38e0b74aae5f7
Alexandru Tenie <alexandru_ale26@yahoo.com>
2024-06-15 18:45:23 +0300

    created client userslice, routes. before adding shacdn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,99 @@
+{
+  // import { BrowserRouter, Routes, Route } from "react-router-dom";
+  // import Home from "./pages/Home";
+  // import About from "./pages/About";
+  // import Dashboard from "./pages/Dashboard";
+  // import NewPost from "./pages/NewPost";
+  // import Login from "./pages/Login";
+  // import Signup from "./pages/Signup";
+  // import Header from "./components/Header";
+  // import Footer from "./components/Footer";
+  // import RouteProtector from "./components/RouteProtector";
+  // const router = createBrowserRouter([]);
+  // function App() {
+  //   return (
+  //     <BrowserRouter>
+  //       <Header />
+  //       <Routes>
+  //         <Route path="/" element={<Home />} />
+  //         <Route path="/login" element={<Login />} />
+  //         <Route path="/signin" element={<Signup />} />
+  //         <Route path="/about" element={<About />} />
+  //         <Route element={<RouteProtector />}>
+  //           <Route path="/dashboard" element={<Dashboard />} />
+  //           <Route path="/posts/new" element={<NewPost />} />
+  //         </Route>
+  //       </Routes>
+  //       <Footer />
+  //     </BrowserRouter>
+  //   );
+  // }
+  // export default App;
+}
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewPost from "./pages/NewPost";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import RouteProtector from "./components/RouteProtector";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Home />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/autentificare",
+    element: <Login />,
+  },
+  {
+    path: "/creeaza-cont",
+    element: <Signup />,
+  },
+  {
+    element: (
+      <>
+        <Header />
+        <RouteProtector />
+        <Footer />
+      </>
+    ),
+    children: [
+      {
+        path: "/administrare",
+        element: <Dashboard />,
+      },
+      {
+        path: "/posts/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+  {
+    path: "/despre",
+    element: (
+      <>
+        <Header />
+        <About />
+        <Footer />
+      </>
+    ),
+  },
+]);
+
 function App() {
-  return <h1 className="text-blue-600">Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/components/RouteProtector.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RouteProtector = () => {
+  return <div>RouteProtector</div>;
+};
+
+export default RouteProtector;
