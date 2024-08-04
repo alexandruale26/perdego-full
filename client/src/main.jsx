a0d09c63c5aa43e0c1ca2ddd9854f6317c5b692c
Alexandru Tenie <alexandru_ale26@yahoo.com>
2024-08-04 20:28:10 +0300

    eliminated useless packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { store, persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PersistGate persistor={persistor}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </PersistGate>
+    <App />
   </React.StrictMode>,
 );
--- client/src/services/signup.js
+++ client/src/services/signup.js
@@ -1,22 +1,3 @@
-import supabase from "./supabase";
-
-const signup = async ({ email, password }) => {
-  try {
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
-
-    if (error) throw new Error(error);
-    return { success: true, data: null };
-  } catch ({ message }) {
-    const alertMessage =
-      message === "AuthApiError: User already registered"
-        ? "Acest e-mail este deja asociat unui cont."
-        : "A apǎrut o problemǎ. Încearcǎ din nou.";
-
-    return { success: false, data: alertMessage };
-  }
+export default async (credentials) => {
+  console.log("signup here", credentials);
 };
-
-export { signup };
